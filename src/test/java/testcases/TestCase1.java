aa9acb349f7186709d30425cc55945ab1e35690f
Raj Kamal <rakamal@bebotechnologies.com>
2022-07-19 13:11:18 +0530

    Jenkins commit 1

--- src/test/java/testcases/TestCase1.java
+++ src/test/java/testcases/TestCase1.java
@@ -1,22 +1,29 @@
 package testcases;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TestCase1 {
 	
+	public WebDriver driver;
 	@Test(priority=1)
 	public void setup() {
-		System.out.println("This is Setup Test...");
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
 	}
 	
 	@Test(priority=2)
-	public void login() {
-		System.out.println("This is Login Test...");
+	public void login() throws InterruptedException {
+		driver.get("https://www.google.com/");
+		Thread.sleep(2000);
 	}
 	
 	@Test(priority=3)
 	public void teardown() {
-		System.out.println("This is Teardown Test...");
+		driver.quit();
 	}
 	
 }
